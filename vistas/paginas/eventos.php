7f823254ea12781295c1941b2322f1e163796f53
nickothan <s1nicolasrg@gmail.com>
2020-04-13 16:05:15 -0500

    traspaso de proyecto a MVC

---
+++ vistas/paginas/eventos.php
@@ -0,0 +1,17 @@
+<main>
+        <h2>Eventos</h2>
+        <p>celebramos con tigo tus fechas especiales</p>
+
+        <div class="cumples">
+            <img src="imagenes/foto cumpleaños familia.jpeg" alt="" width="50%" height="auto">
+            <h3>Cumpleaños</h3>
+        </div>
+
+        <div class="reencuentro">
+            <h3>reencuentro</h3>
+            <img src="imagenes/sele atletismo bogota.jpeg" alt="" width="50%" height="auto">
+        </div>
+
+        div.
+        
+    </main>
---
+++ vistas/paginas/inicio.php
@@ -0,0 +1,27 @@
+<main>
+        <div class="video">
+            <!-- <video src="imagenes/videos/Perro mono.mp4"></video> -->
+            <div class="video-prueba"></div>
+        </div>
+
+        <div class="comidas">
+            <img src="imagenes/Parrillada mona.jpeg" alt="parrillada mona" width="400px" >
+            <img src="imagenes/No joda.jpeg" alt="no joda" width="300px">
+            <img src="imagenes/salchipapa mona.jpeg" alt="salchipapa mona" width="300px">
+        </div>
+
+        <div class="botones">
+            <a href="">Antojate de mas</a>
+            <a href="">Organiza tus eventos con nosotros</a>
+        </div>
+    
+        <div class="eventos">
+            <img src="imagenes/foto cumpleaños familia.jpeg" alt="" width="49%" height="400px">
+            <img src="imagenes/sele atletismo bogota.jpeg" alt="" width="49%" height="400px">
+        </div>
+    
+        <div class="localizacion">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.3473853063065!2d-74.10622948502237!3d4.7095803428913365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f84b4f9bca647%3A0x4ade4d58f6d1f20c!2sPerromono!5e0!3m2!1ses-419!2sar!4v1586642585450!5m2!1ses-419!2sar" width="1024" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
+
+    </main>
