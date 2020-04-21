9964d00b1759cb3f16cb49010e7dd5aa2fbf732f
Revi <oliiv@outlook.com>
2020-04-21 20:03:55 -0300

    sacar un boton de inicio

--- vistas/paginas/inicio.php
+++ vistas/paginas/inicio.php
@@ -29,10 +29,6 @@
                 <img src="imagenes/sele-atletismo-bogota.jpg" alt="">
             </div>
         </div>
-
-        <div>
-            <a href="" class="button"> Como llegar desde aca</a>
-        </div>
     
         <div class="inicio-localizacion">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.3473853063065!2d-74.10622948502237!3d4.7095803428913365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f84b4f9bca647%3A0x4ade4d58f6d1f20c!2sPerromono!5e0!3m2!1ses-419!2sar!4v1586642585450!5m2!1ses-419!2sar" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
